3185e8d97d57be0b7dfc84f4710e790713015025
Nathan LaFreniere <quitlahok@gmail.com>
2013-09-10 15:21:27 -0700

    major rewrite

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 node-github-hook
 ================
 
-This is a very simple, easy to use web hook for github. To use, install with npm and require it in a server process.
+This is a very simple, easy to use evented web hook for github. To use:
 
 ```javascript
-var githubhook = require('githubhook'),
-    servers = {
-        'supersecretpath': 'https://github.com/yourusername/yourrepo',
-        'anotherpath': 'https://github.com/yourusername/yourotherrepo'
-    };
+var githubhook = require('githubhook');
+var github = githubhook({/* options */});
 
-var thishook = githubhook(3000, servers, function (err, payload) {
-    if (!err) {
-        console.log(payload); // payload is the JSON blob that github POSTs to the server
-    } else {
-        console.log(err);
-    }
+github.listen();
+
+github.on('event', function (repo, ref, data) {
+});
+
+github.on('event:reponame', function (ref, data) {
+});
+
+github.on('event:reponame:ref', function (data) {
+});
+
+github.on('reponame', function (event, ref, data) {
+});
+
+github.on('reponame:ref', function (event, data) {
 });
 ```
+
+Where 'event' is the event name to listen to (sent by github, typically 'push'), 'reponame' is the name of your repo (this one is node-github-hook), and 'ref' is the git reference (such as ref/heads/master)
+
+Configure a WebHook URL to whereever the server is listening, with a path of ```/github/callback``` (or ```/github/callback?secret=yoursecret'``` if you set a secret) and you're done!
+
+Available options are:
+
+* host: the host to listen on, defaults to '0.0.0.0'
+* port: the port to listen on, defaults to 3240
+* secret: an optional secret to require in callbacks as a query parameter, default is to not use a secret
+* logger: an optional instance of a logger that supports the "log" and "error" methods and one parameter for data (like console), default is to not log. mostly only for debugging purposes.
 
 License
 =======
--- index.js
+++ index.js
@@ -1,1 +1,138 @@
-module.exports = require('./lib/githubhook.js');
+var Http = require('http');
+var Url = require('url');
+var Querystring = require('querystring');
+var EventEmitter = require('events').EventEmitter;
+var Util = require('util');
+
+var GithubHook = function (options) {
+    if (!(this instanceof GithubHook)) return new GithubHook(options);
+    options = options || {};
+    this.port = options.port || 3420;
+    this.host = options.host || '0.0.0.0';
+    this.secret = options.secret || false;
+    this.logger = options.logger || { log: function () {}, error: function () {} };
+
+    this.server = Http.createServer(serverHandler.bind(this));
+    EventEmitter.call(this);
+};
+
+Util.inherits(GithubHook, EventEmitter);
+
+GithubHook.prototype.listen = function (callback) {
+    var self = this;
+
+    self.server.listen(self.port, self.host, function () {
+        self.logger.log(Util.format('[GithubHook] listening for github events on %s:%d', self.host, self.port));
+        if (typeof callback === 'function') callback();
+    });
+};
+
+function serverHandler(req, res) {
+    var self = this;
+    var url = Url.parse(req.url, true);
+    var buffer = [];
+    var bufferLength = 0;
+    var isForm = false;
+    var failed = false;
+    var remoteAddress = req.ip || req.socket.remoteAddress || req.socket.socket.remoteAddress;
+
+    req.on('data', function (chunk) {
+        if (failed) return;
+        buffer.push(chunk);
+        bufferLength += chunk.length;
+    });
+
+    req.on('end', function (chunk) {
+        if (failed) return;
+        var data;
+
+        if (chunk) {
+            buffer.push(chunk);
+            bufferLength += chunk.length;
+        }
+
+        self.logger.log(Util.format('[GithubHook] received %d bytes from %s', bufferLength, remoteAddress));
+
+        if (req.headers['content-type'] === 'application/x-www-form-urlencoded') isForm = true;
+
+        data = Buffer.concat(buffer, bufferLength).toString();
+        if (isForm) data = Querystring.parse(data).payload;
+        data = parse(data);
+
+        // invalid json
+        if (!data) {
+            self.logger.error(Util.format('[GithubHook] received invalid data from %s, returning 400', remoteAddress));
+            return reply(400, res);
+        }
+
+        var event = req.headers['x-github-event'];
+        var repo = data.repository.name;
+        var ref = data.ref;
+
+        // and now we emit a bunch of data
+        self.logger.log(Util.format('[GithubHook] got %s event on %s:%s from %s', event, repo, ref, remoteAddress));
+        self.emit(repo, event, ref, data);
+        self.emit(repo + ':' + ref, event, data);
+        self.emit(event, repo, ref, data);
+        self.emit(event + ':' + repo, ref, data);
+        self.emit(event + ':' + repo + ':' + ref, data);
+
+        reply(200, res);
+    });
+
+    self.logger.log(Util.format('[GithubHook]', req.method, req.url, remoteAddress));
+
+    // 404 if the path is wrong
+    if (url.pathname !== '/github/callback') {
+        self.logger.error(Util.format('[GithubHook] got invalid path from %s, returning 404', remoteAddress));
+        failed = true;
+        return reply(404, res);
+    }
+
+    // 405 if the method is wrong
+    if (req.method !== 'POST') {
+        self.logger.error(Util.format('[GithubHook] got invalid method from %s, returning 405', remoteAddress));
+        failed = true;
+        return reply(405, res);
+    }
+
+    // 400 if it's not a github event
+    if (!req.headers.hasOwnProperty('x-github-event')) {
+        self.logger.error(Util.format('[GithubHook] missing x-github-event header from %s, returning 400', remoteAddress));
+        failed = true;
+        return reply(400, res);
+    }
+
+    // if a secret is configured, check it and 403 if it's wrong
+    if (self.secret && url.query.secret !== self.secret) {
+        self.logger.error(Util.format('[GithubHook] got invalid secret from %s, returning 403', remoteAddress));
+        failed = true;
+        return reply(403, res);
+    }
+}
+
+function reply(statusCode, res) {
+    var message = { message: Http.STATUS_CODES[statusCode].toLowerCase() };
+    message.result = statusCode >= 400 ? 'error' : 'ok';
+    message = JSON.stringify(message);
+
+    var headers = {
+        'Content-Type': 'application/json',
+        'Content-Length': message.length
+    };
+
+    res.writeHead(statusCode, headers);
+    res.end(message);
+}
+
+function parse(data) {
+    var result;
+    try {
+        result = JSON.parse(data);
+    } catch (e) {
+        result = false;
+    }
+    return result;
+}
+
+module.exports = GithubHook;
